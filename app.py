14a3b89c271066d085924538e65474263c866011
notewio <44951495+notewio@users.noreply.github.com>
2023-11-02 12:07:58 -0400

    cooler research papers page

--- app.py
+++ app.py
@@ -10,9 +10,14 @@ def index():
 def bestpractices():
     return render_template("bestpractices.html")
 
+research_papers_content = [
+    ("ecology",  "AI + Ecology",           "author names here?", "love trees. some summary here"),
+    ("culture",  "AI's Harmonious Fusion", "author names here?", "love words. some summary here"),
+    ("advocacy", "AI Advocacy",            "author names here?", "love words that are directed towards people. some summary here"),
+]
 @app.route('/research_papers')
 def research_papers():
-    return render_template("research_papers.html")
+    return render_template("research_papers.html", papers = research_papers_content)
 
 if __name__ == '__main__':
     app.run(debug=True)
